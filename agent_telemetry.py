d70c96b0407386027d0fcf3900bdc9d2a78d86b7
vcrann <verdon.crann@manchester.ac.uk>
2023-06-20 22:05:48 +0100

    added position reading

--- agent_telemetry.py
+++ agent_telemetry.py
@@ -5,7 +5,8 @@ class AgentTelemetry:
         self._sensor_reading = 0
         self._geodetic_position = (0, 0, 0)
         self._relative_position = (0, 0, 0)
-        self._lock = threading.Lock()
+        self._sensor_reading_lock = threading.Lock()
+        self._geodetic_position_lock = threading.Lock()
     
     def get_sensor_reading(self):
         return self._sensor_reading
@@ -17,11 +18,12 @@ class AgentTelemetry:
         return self._relative_position
     
     def set_sensor_reading(self, sensor_reading):
-        with self._lock:
+        with self._sensor_reading_lock:
             self._sensor_reading = sensor_reading
     
     def set_geodetic_position(self, geodetic_position):
-        self._geodetic_position = geodetic_position
+        with self._geodetic_position_lock:
+            self._geodetic_position = geodetic_position
         
     def set_relative_position(self, relative_position):
         self._relative_position = relative_position
--- message_reader.py
+++ message_reader.py
@@ -4,64 +4,47 @@ import threading
 import time
 from agent_telemetry import AgentTelemetry
 
-
-# def handle_heartbeat(msg):
-#     mode = mavutil.mode_string_v10(msg)
-#     is_armed = msg.base_mode & mavutil.mavlink.MAV_MODE_FLAG_SAFETY_ARMED
-#     is_enabled = msg.base_mode & mavutil.mavlink.MAV_MODE_FLAG_GUIDED_ENABLED
-
-# def read_loop(connection):
-#     while True:
-#         # grab a mavlink message
-#         print("Waiting for message")
-#         msg = connection.recv_match(blocking=False)
-#         if not msg:
-#             print("No message")
-#             return
-
-#         # handle the message based on its type
-#         msg_type = msg.get_type()
-#         if msg_type == "BAD_DATA":
-#             if mavutil.all_printable(msg.data):
-#                 sys.stdout.write(msg.data)
-#                 sys.stdout.flush()
-#         elif msg_type == "RC_CHANNELS_RAW":
-#             handle_rc_raw(msg)
-#         elif msg_type == "HEARTBEAT":
-#             handle_heartbeat(msg)
-#         elif msg_type == "VFR_HUD":
-#             handle_hud(msg)
-#         elif msg_type == "ATTITUDE":
-#             handle_attitude(msg)
-
-def read_loop(connection, agent):
+def get_sensor_reading(connection, agent):
     while True:
         sensor_reading_msg = connection.recv_match(type='NAMED_VALUE_FLOAT', blocking=True)
         agent.set_sensor_reading(sensor_reading_msg.value)
-        print(sensor_reading_msg.name)
+        
+def get_geodetic_position(connection, agent):
+    while True:
+        geodetic_position_msg = connection.recv_match(type='GLOBAL_POSITION_INT', blocking=True)
+        agent.set_geodetic_position((geodetic_position_msg.lat, geodetic_position_msg.lon, geodetic_position_msg.alt))
         
 def print_loop(agent):
     while True:
         print("CO reading: ", agent.get_sensor_reading())
+        print("Geodetic position: ", agent.get_geodetic_position())
         time.sleep(1)
 
 
 def main():
     agent = AgentTelemetry()
-    system_address = "/dev/ttyACM0"  # default address for SITL
+    #system_address = "/dev/ttyACM0" #direct USB to pixhawk
+    system_address = "/dev/ttyUSB1" #USB to telemetry radio
     connection = mavutil.mavlink_connection(system_address, 57600)
 
     # Wait for the first heartbeat
     connection.wait_heartbeat()
     print("Heartbeat from system (system %u component %u)" % (connection.target_system, connection.target_component))
-
-    # enter the data loop
-    read_thread = threading.Thread(target=read_loop, args=(connection, agent,))
-    read_thread.start()
     
+    # Request all parameters
+    connection.mav.request_data_stream_send(connection.target_system, connection.target_component, 
+	mavutil.mavlink.MAV_DATA_STREAM_ALL, 1, 1)
+
+    # Spawn threads to read sensor readings and geodetic position
+    sensor_reading_thread = threading.Thread(target=get_sensor_reading, args=(connection, agent,))
+    sensor_reading_thread.start()
+    
+    geodetic_position_thread = threading.Thread(target=get_geodetic_position, args=(connection, agent,))
+    geodetic_position_thread.start()
+    
+    # Spawn thread to print sensor readings and geodetic position
     print_thread = threading.Thread(target=print_loop, args=(agent,))
     print_thread.start()
-    #read_loop(connection, agent)
 
 
 if __name__ == "__main__":
