2e0ac232eb30ad6af7882308f4334c9251d568dc
JoshuaTheKindaEngineer <Joshuasgw@gmail.com>
2023-06-21 21:06:17 +0100

    Naming convention

--- chemotaxis.py
+++ chemotaxis.py
@@ -10,18 +10,18 @@ class Chemotaxis:
         self.D = D
         self.phi = np.random.rand() * np.pi
     def update(
-        self, new_conc, current_North, current_East, current_Down
+        self, new_conc, relative_position
     ):  # maybe change to using NED for this or we are gonna get ourselves confused
         if new_conc > self.conc:
-            new_North = current_North + np.cos(self.theta) * np.sin(self.phi) * self.D
-            new_East = current_East + np.sin(self.theta) * np.sin(self.phi) * self.D
-            new_Down = current_Down + np.cos(self.phi) * self.D
+            new_North = relative_position + np.cos(self.theta) * np.sin(self.phi) * self.D
+            new_East = relative_position + np.sin(self.theta) * np.sin(self.phi) * self.D
+            new_Down = relative_position + np.cos(self.phi) * self.D
         else:
             self.theta = np.random.rand() * 2 * np.pi
             self.phi = np.random.rand() * np.pi
-            new_North = current_North + np.cos(self.theta) * np.sin(self.phi) * self.D
-            new_East = current_East + np.sin(self.theta) * np.sin(self.phi) * self.D
-            new_Down = current_Down + np.cos(self.phi) * self.D
+            new_North = relative_position[0] + np.cos(self.theta) * np.sin(self.phi) * self.D
+            new_East = relative_position[1] + np.sin(self.theta) * np.sin(self.phi) * self.D
+            new_Down = relative_position[2] + np.cos(self.phi) * self.D
         self.conc = new_conc
 
         return new_North, new_East, new_Down
